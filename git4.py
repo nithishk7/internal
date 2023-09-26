c0a5b86475edc6053a77edfc00cd3068d91d0138
nithishk7 <snithishk7@gmail.com>
2023-09-26 14:41:31 +0530

    file are added

---
+++ git4.py
@@ -0,0 +1,10 @@
+a=int((input enter a number :))
+b=int((input enter a number :))
+if(a>b):
+    print ("a is greater")
+elif(a<b):
+    print("b is greater")
+else:
+    print("both are same ")
+
+
